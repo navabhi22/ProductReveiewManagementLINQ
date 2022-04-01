25ea2d65190368e0abab188cf03931a480db28ac
navabhi22 <navabhi22@gmail.com>
2022-04-01 21:35:37 +0530

    Greeting message fot  Produt Review Management Program

---
+++ ProductReviewManagementLINQ/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagementLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management Problems Using LINQ");
+        }
+    }
+}
